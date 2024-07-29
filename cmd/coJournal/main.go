edbf55916e0e1b69fd95df9ae685dc69ebf73f24
pongpatapee <pongpatapee1021@gmail.com>
2024-07-29 15:14:18 -0400

    feat: grouped user routes

--- cmd/coJournal/main.go
+++ cmd/coJournal/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"coJournal/internal/repository/postgres"
+	"coJournal/internal/server"
 	"coJournal/internal/server/handler"
 	"coJournal/internal/service"
 	"context"
@@ -53,27 +54,12 @@ func main() {
 		return c.String(http.StatusOK, "Welcome to CoJournal!")
 	})
 
-	// apiGroup := e.Group("/api")
-	// apiGroup.GET("", func(c echo.Context) error {
-	// 	return c.String(http.StatusOK, "what the fuck?")
-	// })
-	//
-	// userRepo := in_memory.NewInMemoryUserRepository()
+	apiRouter := e.Group("/api")
+
 	userRepo := postgres.NewPostgresUserRepository(dbpool)
 	userService := service.NewUserService(userRepo)
 	userHandler := handler.NewUserHTTPHandler(userService)
-	// userGroup := apiGroup.Group("/user")
-	// userGroup.POST("", userHandler.CreateUser)
-	// userGroup.GET("", userHandler.GetAllUser)
-	// userGroup.GET(":id", userHandler.GetUser)
-	// userGroup.PUT(":id", userHandler.UpdateUser)
-	// userGroup.DELETE(":id", userHandler.DeleteUser)
-
-	e.POST("/api/user", userHandler.CreateUser)
-	e.GET("/api/user", userHandler.GetAllUser)
-	e.GET("/api/user/:id", userHandler.GetUser)
-	e.PUT("/api/user/:id", userHandler.UpdateUser)
-	e.DELETE("/api/user/:id", userHandler.DeleteUser)
+	server.RegisterUserRoutes(apiRouter, userHandler)
 
 	e.Logger.Fatal(e.Start(":8000"))
 }
--- internal/server/router.go
+++ internal/server/router.go
@@ -1,52 +1,24 @@
 package server
 
-// import (
-// 	"coJournal/internal/repository/in_memory"
-// 	"coJournal/internal/server/handler"
-// 	"coJournal/internal/service"
-// 	"fmt"
-// 	"net/http"
-// )
+import (
+	"coJournal/internal/server/handler"
+	"fmt"
 
-// // TODO: group routes and setup server in server.go, injecting configuration
-// func SetupRouter() *http.ServeMux {
-// 	r := http.NewServeMux()
+	"github.com/labstack/echo/v4"
+)
 
-// 	r.HandleFunc("GET /", func(w http.ResponseWriter, r *http.Request) {
-// 		fmt.Fprint(w, "Welcome to CoJournal!")
-// 	})
+func RegisterUserRoutes(apiRouter *echo.Group, userHandler *handler.UserHTTPHandler) {
+	fmt.Println("Registering user Routes!")
+	userRouter := apiRouter.Group("/user")
+	userRouter.POST("", userHandler.CreateUser)
+	userRouter.GET("", userHandler.GetAllUser)
+	userRouter.GET("/:id", userHandler.GetUser)
+	userRouter.PUT("/:id", userHandler.UpdateUser)
+	userRouter.DELETE("/:id", userHandler.DeleteUser)
+}
 
-// 	// TODO: Refactor with register routes or something
+func RegisterJournalRoutes(apiRouter *echo.Group, userHandler *handler.JournalHTTPHandler) {
+}
 
-// 	userRepo := in_memory.NewInMemoryUserRepository()
-// 	userService := service.NewUserService(userRepo)
-// 	userHandler := handler.NewUserHTTPHandler(userService)
-
-// 	r.HandleFunc("POST /api/user", userHandler.CreateUser)
-// 	r.HandleFunc("GET /api/user", userHandler.GetAllUser)
-// 	r.HandleFunc("GET /api/user/{id}", userHandler.GetUser)
-// 	r.HandleFunc("PUT /api/user/{id}", userHandler.UpdateUser)
-// 	r.HandleFunc("DELETE /api/user/{id}", userHandler.DeleteUser)
-
-// 	journalRepo := in_memory.NewInMemoryJournalRepository()
-// 	journalService := service.NewJournalService(journalRepo)
-// 	journalHandler := handler.NewJournalHTTPHandler(journalService)
-
-// 	r.HandleFunc("POST /api/journal", journalHandler.CreateJournal)
-// 	r.HandleFunc("GET /api/journal", journalHandler.GetAllJournal)
-// 	r.HandleFunc("GET /api/journal/{id}", journalHandler.GetJournal)
-// 	r.HandleFunc("PUT /api/journal/{id}", journalHandler.UpdateJournal)
-// 	r.HandleFunc("DELETE /api/journal/{id}", journalHandler.DeleteJournal)
-
-// 	noteRepo := in_memory.NewInMemoryNoteRepository()
-// 	noteService := service.NewNoteService(noteRepo)
-// 	noteHandler := handler.NewNoteHTTPHandler(noteService)
-
-// 	r.HandleFunc("POST /api/note", noteHandler.CreateNote)
-// 	r.HandleFunc("GET /api/note", noteHandler.GetAllNote)
-// 	r.HandleFunc("GET /api/note/{id}", noteHandler.GetNote)
-// 	r.HandleFunc("PUT /api/note/{id}", noteHandler.UpdateNote)
-// 	r.HandleFunc("DELETE /api/note/{id}", noteHandler.DeleteNote)
-
-// 	return r
-// }
+func RegisterNoteRoutes(apiRouter *echo.Group, NoteHandler *handler.NoteHTTPHandler) {
+}
